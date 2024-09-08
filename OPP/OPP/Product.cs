03365ce12fef50804e688599e2d6a353c99c0fb9
bich samrouth <125858166+samrouth1234@users.noreply.github.com>
2024-09-08 02:58:18 -0600

    Add files via upload
    OOP C# Programming

---
+++ OPP/OPP/Product.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+
+public class Product
+{
+
+    // create file 
+    int product_id;
+    string product_name;
+    string product_description;
+    decimal price;
+
+    // construtor
+
+    //public Product(int product_id, string product_name , string product_description , decimal price)
+    //{
+    //    this.product_id = product_id;
+    //    this.product_name = product_name;
+    //    this.product_description = product_description;
+    //    this.price = price;
+    //}
+
+    // create get set
+    public int ProductID // property
+    {
+        get { return product_id; }
+        set { product_id = value; }
+    }
+
+    public string ProductName  // property
+    {
+        get { return product_name; }
+        set { product_name = value; }
+    }
+
+    public string ProductDescription // property
+    {
+        get { return product_description; }
+        set { product_description = value; }
+    }
+
+    public decimal Price // property
+    {
+        get { return price; }
+        set { price = value; }
+    }
+
+    // Input Prodcut
+
+    public void InputProduct()
+    {
+        Console.Write("Enter Product ID :");
+        product_id = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Product Name :");
+        product_name = Console.ReadLine();
+        Console.Write("Enter Product Description :");
+        product_description = Console.ReadLine();
+        Console.Write("Enter Product Price :");
+        price = Convert.ToDecimal(Console.ReadLine());
+    }
+
+    // show product 
+    public void ShowProduct()
+    {
+        Console.WriteLine($"ID: {product_id,5}");
+        Console.WriteLine($"ProductName: {product_name,-20}");
+        Console.WriteLine($"ProductDescription: {product_description,-30}");
+        Console.WriteLine($"Price: {price,10:C}"); // :C = $
+    }
+}
---
+++ OPP/OPP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            int n = 0;
+            Console.WriteLine("----------------------------------------");
+            Console.Write("Enter Number :");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine("------------ INPUT PRODUCT -------------");
+            for (int i = 0; i < n; i++) {
+                product.InputProduct();
+                Console.WriteLine("----------------------------------------");
+            }
+
+            Console.WriteLine("------------ SHOW PRODUCT --------------");
+            for (int i = 0; i < n; i++)
+            {
+                product.ShowProduct();
+            }
+            Console.WriteLine("---------------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
